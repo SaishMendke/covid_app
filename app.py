9c1e94810486f9afe27754c3bf63e4c334f79a52
Saish Mendke <56043994+SaishMendke@users.noreply.github.com>
2020-08-01 20:37:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+import pandas as pd
+import sklearn
 
 app = Flask(__name__) #Initialize the flask App
-model = pickle.load(open('model.pkl', 'rb'))
+#model = pickle.load(open('model.pkl', 'rb'))
+
+data = pd.read_csv('data(1).csv')
+
+x = data.iloc[:, :6]
+y = data.iloc[:, -1]
+
+from sklearn.linear_model import LogisticRegression
+regressor = LogisticRegression()
+
+#Fitting model with trainig data
+regressor.fit(x, y)
 
 @app.route('/')
 def home():
@@ -16,12 +29,12 @@ def predict():
     '''
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    prediction = regressor.predict(final_features)
     #prob = model.predict_proba(final_features)
 
     output1 = round(prediction[0], 2)
     
     return render_template('index.html', prediction_text='The prediction of COVID-19 is {}'.format(output1))
 
-#if __name__ == "__main__":
- #   app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
